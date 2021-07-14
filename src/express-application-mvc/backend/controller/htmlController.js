c7482bd2b4557922e3ca9f31775bca13281a992b
unknown <shubhanshu.rai94@gmail.com>
2021-07-14 13:28:29 +0530

    RoutingandNavBarFix

--- src/express-application-mvc/backend/controller/htmlController.js
+++ src/express-application-mvc/backend/controller/htmlController.js
@@ -1,10 +1,75 @@
 const path = require("path");
 
-let indexPage = (req,res) => {
+
+let abouthospital = (req,res) => {
+    res.render("about-hospital");
+};
+
+let aboutus = (req,res) => {
+    res.render("about-us");
+};
+
+let appointment = (req,res) => {
+    res.render("appointment");
+};
+
+let contactUs = (req,res) => {
+    res.render("contactUs");
+};
+
+let doctorProfile = (req,res) => {
+    res.render("doctor-profile");
+};
+
+let doctor = (req,res) => {
+    res.render("doctor");
+};
+
+let FAQ = (req,res) => {
+    res.render("FAQ");
+};
+
+let hospital = (req,res) => {
+    res.render("hospital");
+};
+
+let index = (req,res) => {
     res.render("index");
 };
 
+let login = (req,res) => {
+    res.render("login");
+};
+
+let query = (req,res) => {
+    res.render("query");
+};
+
+let signup = (req,res) => {
+    res.render("signup");
+};
+
+let Treatment = (req,res) => {
+    res.render("Treatment");
+};
+
+let tvastraPlus = (req,res) => {
+    res.render("tvastra-plus");
+};
 
 module.exports = {
-    indexPage : indexPage,
+    abouthospital : abouthospital,
+    aboutus : aboutus,
+    appointment : appointment,
+    contactUs : contactUs,
+    doctorProfile : doctorProfile,
+    doctor : doctor,
+    FAQ : FAQ,
+    hospital : hospital,
+    index : index,
+    login : login,
+    query : query,
+    signup : signup,
+    Treatment : Treatment,
+    tvastraPlus : tvastraPlus,
 };
--- src/express-application-mvc/backend/routes/htmlRoutes.js
+++ src/express-application-mvc/backend/routes/htmlRoutes.js
@@ -2,6 +2,20 @@ const express = require("express");
 const pages = require("../controller/htmlController");
 const router = express.Router();
 
-router.route("/").get(pages.indexPage);
+router.route("/about-hospital").get(pages.abouthospital);
+router.route("/about-us").get(pages.aboutus);
+router.route("/appointment").get(pages.appointment);
+router.route("/contactUs").get(pages.contactUs);
+router.route("/doctor-profile").get(pages.doctorProfile);
+router.route("/doctor").get(pages.doctor);
+router.route("/FAQ").get(pages.FAQ);
+router.route("/hospital").get(pages.hospital);
+router.route("/").get(pages.index);
+router.route("/login").get(pages.login);
+router.route("/query").get(pages.query);
+router.route("/signup").get(pages.signup);
+router.route("/Treatment").get(pages.Treatment);
+router.route("/tvastra-plus").get(pages.tvastraPlus);
+
 
 module.exports = router ;
